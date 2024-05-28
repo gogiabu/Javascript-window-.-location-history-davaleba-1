388451690a67656ec29fb6eef12e3f577340f79e
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-05-28 19:27:35 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+document.write(window.location.href + ("<br>"));
+
+function w3schools() {
+
+    location = "https://www.w3schools.com/default.asp";
+}
+
+function reload() {
+
+    window.location.reload(); 
+}
+
+
+        
+
+
+
+     
+
+
+       
